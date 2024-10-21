a0c1180393c5220e47a0321ccb372c99d1f96bfb
Rui Xiang <rxiang@nelson.msl.ubc.ca>
2024-10-21 16:47:10 -0700

    started working on SZ age distributions

---
+++ R/4-szagedistributionandumap.R
@@ -0,0 +1,27 @@
+# Author: Rui Xiang Yu
+# Date: 2024 October 21st
+# This script obtains the number of single cells and genes in the Schizophrenia cohorts.
+# Usage: R/4-szagedistributionandumap.R
+
+library(tidyverse)
+library(ggplot2)
+library(RColorBrewer)
+library(cowplot)
+
+main <- function() {
+  CMC_meta <- readRDS("data/data_processed/CMC/CMC-patients.rds") |>
+    mutate(age = as.numeric(str_replace(age, "\\+", "")))
+  MB_meta <- readRDS("data/data_processed/MultiomeBrain/MultiomeBrain-patients.rds") |>
+    mutate(age = as.numeric(str_replace(age, "\\+", "")))
+  SZBD_meta <- readRDS("data/data_processed/SZBDMulti-Seq/SZBDMulti-Seq-patients.rds") |>
+    mutate(age = as.numeric(str_replace(age, "\\+", "")))
+  
+  CMC_age_hist <- CMC_meta |>
+    mutate(disorder = recode(disorder, "yes" = "Schizophrenia", "no" = "Control")) |>
+    ggplot(aes(x = age)) +
+    geom_histogram()
+  
+  print(CMC_age_hist)
+}
+
+main()
