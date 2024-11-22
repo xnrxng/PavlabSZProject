19e79fb695a7ae90a6e72fcd431373534b1099d3
Rui Xiang <rxiang@nelson.msl.ubc.ca>
2024-11-22 13:14:39 -0800

    finished tmm with covs dea

--- R/6-pseudobulkanddea.R
+++ R/6-pseudobulkanddea.R
@@ -174,6 +174,33 @@ main <- function() {
     }
   }
   
+  ### DEA on TMM with other covariates
+  for (cohort in cohort_list){
+    astrocyte <- paste0("Ast_", cohort, "_SZ.rds")
+    excitatory <- paste0("Exc_", cohort, "_SZ.rds")
+    inhibitory <- paste0("Inh_", cohort, "_SZ.rds")
+    microglia <- paste0("Mic_", cohort, "_SZ.rds")
+    oligodendrocyte <- paste0("Oli_", cohort, "_SZ.rds")
+    opc <- paste0("Opc_", cohort, "_SZ.rds")
+    gli <- paste0("Gli_", cohort, "_SZ.rds")
+    
+    cell_list <- c(astrocyte, excitatory, inhibitory, microglia, oligodendrocyte, opc, gli)
+    
+    for (cell in cell_list) {
+      pb_path <- paste0("data/data_processed/", cohort, "/Pseudobulk/PseudobulkRaw/", cell)
+      
+      if (!file.exists(pb_path)) {
+        message("File ", pb_path, " does not exist. Skipping to next.")
+        next
+      }
+      
+      pseudobulk <- readRDS(pb_path)
+      dea_res <- perform_DGE_with_covs_on_tmm(pseudobulk)
+      
+      dea_path <- paste0("results/DEA/", cohort, "/TMMWithCovariates/DEAresults_", cell)
+      saveRDS(dea_res, dea_path)
+    }
+  }
   
   ### create p-value hists
   
@@ -360,6 +387,69 @@ main <- function() {
       all_plots_with_title <- plot_grid(title_plot, all_plots, ncol = 1, rel_heights = c(0.1, 1))
       
       final_path <- paste0("results/DEA/", cohort, "/TMM/pvalueplot_", cohort, ".png")
+      
+      cowplot::save_plot(plot = all_plots_with_title, filename = final_path)
+      
+    }
+  }
+  
+  for (cohort in cohort_list){
+    astrocyte <- paste0("Ast_", cohort, "_SZ.rds")
+    excitatory <- paste0("Exc_", cohort, "_SZ.rds")
+    inhibitory <- paste0("Inh_", cohort, "_SZ.rds")
+    microglia <- paste0("Mic_", cohort, "_SZ.rds")
+    oligodendrocyte <- paste0("Oli_", cohort, "_SZ.rds")
+    opc <- paste0("Opc_", cohort, "_SZ.rds")
+    gli <- paste0("Gli_", cohort, "_SZ.rds")
+    
+    cell_list <- c(astrocyte, excitatory, inhibitory, microglia, oligodendrocyte, opc, gli)
+    plot_list <- list()
+    
+    for (cell in cell_list) {
+      res_path <- paste0("results/DEA/", cohort, "/CPMLogWithCovariates/DEAresults_", cell)
+      
+      cell_type <- strsplit(cell, "_")[[1]][1]
+      
+      if (!file.exists(res_path)) {
+        message("File ", res_path, " does not exist. Skipping to next.")
+        next
+      }
+      results <- readRDS(res_path)
+      
+      res_his <- results |>
+        ggplot(aes(x = PValue)) +
+        geom_histogram() +
+        theme_classic()+
+        labs(
+          title = cell_type,
+          x = "P-value",
+          y = NULL
+        )
+      
+      plot_list[[cell_type]] <- res_his
+    }
+    
+    cohort_title <- paste0(cohort, ": CPMLog with Age and Sex")
+    
+    all_plots <- plot_grid(plotlist = plot_list, ncol = 3, align = "hv")
+    
+    title_plot <- ggdraw() + 
+      draw_label(cohort_title, x = 0.5, y = 0.5, size = 12, hjust = 0.5)
+    
+    if (cohort == "Batiuk") {
+      
+      all_plots_with_title <- plot_grid(title_plot, all_plots, ncol = 1, rel_heights = c(1, 1))
+      
+      final_path <- paste0("results/DEA/", cohort, "/CPMLogWithCovariates/pvalueplot_", cohort, ".png")
+      
+      cowplot::save_plot(plot = all_plots_with_title, filename = final_path)
+      
+    }
+    
+    else{
+      all_plots_with_title <- plot_grid(title_plot, all_plots, ncol = 1, rel_heights = c(0.1, 1))
+      
+      final_path <- paste0("results/DEA/", cohort, "/CPMLogWithCovariates/pvalueplot_", cohort, ".png")
       
       cowplot::save_plot(plot = all_plots_with_title, filename = final_path)
       
@@ -856,6 +946,33 @@ perform_DGE_with_covs <- function(PB) {
   design <- model.matrix(~ group + sex + age, data = PB$meta)
   y <- DGEList(counts = PB$expr, group = PB$meta$group)
   
+  x <- estimateDisp(y, design, trend.method = "locfit", tagwise = TRUE, prior.df = NULL)   # Estimate dispersion
+  fit <- glmFit(x, design = design)   # Fit GLM (edgeR)
+  test <- glmLRT(fit)   # Likelihood ratio test
+  
+  # Extract results
+  res <- topTags(test, n = Inf) %>%
+    as.data.frame() %>%
+    rownames_to_column('gene') %>%
+    mutate(test = 'pseudobulk_edgeR')
+  
+  return(res)
+}
+
+perform_DGE_with_covs_on_tmm <- function(PB) {
+  # Check if all sample names in the expression matrix match group sample names
+  if (!all(colnames(PB$expr) %in% PB$meta$group_sample)) {
+    stop("Sample names in expression matrix do not match group sample names in meta data")
+  }
+  
+  PB$meta$age <- gsub("\\+", "", PB$meta$age)
+  PB$meta$age <- as.numeric(PB$meta$age)
+  PB$meta$sex <- as.factor(PB$meta$sex)
+  
+  ### make it return tmm normalized matrix
+  design <- model.matrix(~ group, data = PB$meta)
+  y <- DGEList(counts = PB$expr, group = PB$meta$group)
+  y <- calcNormFactors(y, method = "TMM")  # Normalization
   x <- estimateDisp(y, design, trend.method = "locfit", tagwise = TRUE, prior.df = NULL)   # Estimate dispersion
   fit <- glmFit(x, design = design)   # Fit GLM (edgeR)
   test <- glmLRT(fit)   # Likelihood ratio test
